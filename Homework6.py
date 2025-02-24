57f3e87bde2b68cdb1d883e06e6ba1d54a533198
DivitPython-11 <divitvakilhk@gmail.com>
2025-02-24 20:53:11 +0800

    Add files via upload

---
+++ Homework6.py
@@ -0,0 +1,14 @@
+#First Activity
+a=ord(input("Enter any character: "))
+print(a)
+if a>=65 and a<=90 or a>=97 and a<=122:
+    print("This is an alphabet")
+else: 
+    print("This is not an alphabet")
+
+#Second Activity 
+year=int(input("Enter any year:  "))
+if year%4 !=0:
+    print("This is not a leap year")
+else:
+    print(" This is a leap year")
